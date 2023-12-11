f1631edd2c5e5b6f6796769fc2d21c7f740bb075
Oksanyte <153068083+Oksanyte@users.noreply.github.com>
2023-12-11 23:51:08 +0200

    Update main.py
    Perrašiau kodą naujai

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import re
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0"
@@ -22,45 +23,43 @@ data = []
 
 
 # define website url
-URL = f"https://www.imdb.com/search/title/?release_date=2020-01-01,2023-12-31&sort=boxoffice_gross_us,desc"
+URL = f"https://www.imdb.com/list/ls503325184/"
 driver.get(URL)
+
 def paspausti():
-    load_more = WebDriverWait(driver, 45).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, '.ipc-see-more__text > '))
+    load_more = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '.lister-page-next.next-page'))
     )
     driver.execute_script("arguments[0].scrollIntoView();", load_more)
     load_more.click()
-    time.sleep(4)
-
-# Atsidarius puslapi yra nuscrolinama i apacia ir palaukiama 3 sekundes
-driver.execute_script('window.scrollBy(0, 12000);')
-time.sleep(3)
+    time.sleep(10)
+#
+# # Atsidarius puslapi yra nuscrolinama i apacia ir palaukiama 3 sekundes
+driver.execute_script('window.scrollBy(0, 30000);')
+time.sleep(10)
 # sukam for cikla siuo atveju 50 kartu ir spaudziamas mygtukas kad uzkrauti daugiau duomenu
-for i in range(1, 5):
+for _ in range(1, 3):
     paspausti()
 
-response = requests.get(URL, headers=headers)
+
 soup = BeautifulSoup(driver.page_source, 'html.parser')
-print(response)
-print(soup)
-movies = soup.find_all('div', class_='sc-53c98e73-4 gOfInm dli-parent')
+# print(response)
+# print(soup)
+movies = soup.find_all('div', class_='lister-item-content')
 # print(movies)
 new_movies_list = []
-
+#
 for movie in movies:
-    title = movie.find('h3', class_='ipc-title__text').text.strip()
-    years = movie.find('div', class_='sc-43986a27-7 dBkaPT dli-title-metadata').text.strip()[0:4]
-    length = movie.find('div', class_='sc-43986a27-7 dBkaPT dli-title-metadata').text.strip()[4:10]
-    certificate = movie.find('div', class_='sc-43986a27-7 dBkaPT dli-title-metadata').text.strip()[10:16]
-    # genre = movie.find('span', class_='genre').text.strip()
-    rating = movie.find('span', class_= 'ipc-rating-star ipc-rating-star--base ipc-rating-star--imdb ratingGroup--imdb-rating').text.strip()[0:3]
-    vote_count = movie.find('span', class_= 'ipc-rating-star ipc-rating-star--base ipc-rating-star--imdb ratingGroup--imdb-rating').text.strip()[5:9].replace('K', '000')
-    metascore = movie.find('span', class_='sc-43986a27-11 QigWJ').text.strip()
-    votes = movie.find('div', class_='sc-53c98e73-0 kRnqtn').text.replace('Votes', '').strip()
-    new_movies_list.append((title, years,length, certificate, rating, vote_count, metascore, votes))
-
-
-
-print(new_movies_list)
-# df = pd.DataFrame(new_movies_list)
-# # print(df)
+    title = movie.find('h3', class_='lister-item-header').text.strip()
+    title_text = re.search(r"\n(.+)\n", title).group(1)
+    years = movie.find('span', class_='lister-item-year text-muted unbold').text.strip().replace("(", "").replace(")", "")
+    length = movie.find('span', class_='runtime').text.strip().replace('min', '')
+    genre = movie.find('span', class_='genre').text.strip()
+    rating = movie.find('span', class_='ipl-rating-star__rating').text.strip()
+    new_movies_list.append({'Pavadinimas': title_text, 'Metai': years, 'Trukmė': length, 'Žanras':genre, 'Reitingas':rating})
+#
+# # print(new_movies_list)
+#
+lentele = pd.DataFrame(new_movies_list)
+print(lentele)
+lentele.to_csv('baigiamasis.csv', index=False)
